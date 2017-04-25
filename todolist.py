ec2bd01d106910253d135cc362d809a930c14d1f
lydialim <5litez@gmail.com>
2017-04-25 10:08:28 +0800

    Update to follow Python style guide

--- todolist.py
+++ todolist.py
@@ -2,60 +2,60 @@
 
 from todoitem import *
 
-def add(todoList):
+def add(todo_list):
     # Adds an item to the list
     print("\nEnter your to-do:")
     text = input()
 
-    if not todoList:
+    if not todo_list:
         lastId = 0
     else:
         try:
-            lastId = todoList[-1].id
+            lastId = todo_list[-1].id
         except IndexError:
             # No items in list
             lastId = 0
 
-    newItem = TodoItem(lastId + 1, text)
-    todoList.append(newItem)
+    new_item = TodoItem(lastId + 1, text)
+    todo_list.append(new_item)
 
-    print("\n%s is added" % newItem.text)
+    print("\n%s is added" % new_item.text)
 
-def remove(todoList):
+def remove(todo_list):
     print("\nEnter a todo item Id:")
-    itemId = int(input())
+    item_id = int(input())
 
     # removes a todo item by id
-    item = todoList[itemId - 1]
-    del todoList[itemId - 1]
+    item = todo_list[item_id - 1]
+    del todo_list[item_id - 1]
     print("\n%s is deleted" % item.text)
 
-def markDone(todoList):
+def mark_done(todo_list):
     print("\nEnter a todo item Id:")
-    itemId = int(input())
+    item_id = int(input())
 
     # mark a todo item as done
-    item = todoList[itemId - 1]
+    item = todo_list[item_id - 1]
     item.done = True
     print("\n%s is done" % item.text)
 
 def create(name):
     # creates a new todo object
-    myTodo = {'Owner' : name, 'List': []}
-    return myTodo
+    todo_book = {'Owner' : name, 'List': []}
+    return todo_book
 
-def display(myTodo):
+def display(todo_book):
     # prints the todo list
-    print("\n%s's To-Do" % myTodo['Owner'])
-    todoList = myTodo['List']
+    print("\n%s's To-Do" % todo_book['Owner'])
+    todo_list = todo_book['List']
 
     # check if todo list is empty
-    if not todoList:
+    if not todo_list:
         print("Your to-do list is empty.")
         return
 
     # print todo list
-    for todo in todoList:
+    for todo in todo_list:
         print (todo)
 
 def menu(name):
@@ -76,20 +76,20 @@ def run():
     name = input()
 
     # The list
-    myTodo = create(name)
-    myTodoList = myTodo['List']
+    todo_book = create(name)
+    todo_list = todo_book['List']
 
     while True:
-        choice = menu(myTodo['Owner'])
+        choice = menu(todo_book['Owner'])
 
         if choice == 1:
-            display(myTodo)
+            display(todo_book)
         elif choice == 2:
-            add(myTodoList)
+            add(todo_list)
         elif choice == 3:
-            markDone(myTodoList)
+            mark_done(todo_list)
         elif choice == 4:
-            remove(myTodoList)
+            remove(todo_list)
         elif choice == 0:
             break
 
